c817a598ed409aae2ef77f1bb4ba6344ed9d3977
omerzam <omerzamsky@gmail.com>
2021-01-04 16:48:49 +0200

    removed some comments

--- fuse/src/one-time-payment-mapping.ts
+++ fuse/src/one-time-payment-mapping.ts
@@ -20,18 +20,10 @@ export function handlePaymentWithdraw(event: PaymentWithdraw): void {
       citizenTo = new Citizen(event.params.to.toHex())
     }
 
-    // citizenTo.inTransactions = citizenTo.inTransactions.plus(BigInt.fromI32(1))
     citizenTo.inTransactionsClean = citizenTo.inTransactionsClean.plus(BigInt.fromI32(1))
-
-    // citizenTo.inTransactionsValue = citizenTo.inTransactionsValue.plus(event.params.amount)
     citizenTo.inTransactionsValueClean = citizenTo.inTransactionsValueClean.plus(event.params.amount)
-
-    // citizenTo.totalTransactions = citizenTo.totalTransactions.plus(BigInt.fromI32(1))
     citizenTo.totalTransactionsClean = citizenTo.totalTransactionsClean.plus(BigInt.fromI32(1))
-
-    // citizenTo.totalTransactionsValue = citizenTo.totalTransactionsValue.plus(event.params.amount)
     citizenTo.totalTransactionsValueClean = citizenTo.totalTransactionsValueClean.plus(event.params.amount)
-
     citizenTo.save()
 
     let citizenFrom = Citizen.load(event.params.from.toHex())
@@ -39,18 +31,10 @@ export function handlePaymentWithdraw(event: PaymentWithdraw): void {
       citizenFrom = new Citizen(event.params.from.toHex())
     }
 
-    // citizenFrom.outTransactions = citizenFrom.outTransactions.plus(BigInt.fromI32(1))
     citizenFrom.outTransactionsClean = citizenFrom.outTransactionsClean.plus(BigInt.fromI32(1))
-
-    // citizenFrom.outTransactionsValue = citizenFrom.outTransactionsValue.plus(event.params.amount)
     citizenFrom.outTransactionsValueClean = citizenFrom.outTransactionsValueClean.plus(event.params.amount)
-
-    // citizenFrom.totalTransactions = citizenFrom.totalTransactions.plus(BigInt.fromI32(1))
     citizenFrom.totalTransactionsClean = citizenFrom.totalTransactionsClean.plus(BigInt.fromI32(1))
-
-    // citizenFrom.totalTransactionsValue = citizenFrom.totalTransactionsValue.plus(event.params.amount)
     citizenFrom.totalTransactionsValueClean = citizenFrom.totalTransactionsValueClean.plus(event.params.amount)
-
     citizenFrom.save()
   }
 }
