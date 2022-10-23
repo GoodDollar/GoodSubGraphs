ff963d8b5884e55e3e1158f2bbd24cfecce55e42
sirpy <hadarr@gmail.com>
2022-10-23 13:19:22 +0300

    fix: missing protocol contracts

--- fuse/scripts/get-contract-releases.js
+++ fuse/scripts/get-contract-releases.js
@@ -1,63 +1,46 @@
 const { readFileSync, writeFileSync } = require('fs')
 const path = require('path')
 const merge = require('lodash.merge')
+const flatten = require('lodash').flatten
+const uniq = require('lodash').uniq
 
 const c1 = require('@gooddollar/goodcontracts/releases/deployment.json')
 const c2 = require('@gooddollar/goodcontracts/stakingModel/releases/deployment.json')
 const c3 = require('@gooddollar/goodcontracts/upgradables/releases/deployment.json')
 const c4 = require('@gooddollar/goodprotocol/releases/deployment.json')
+let mergedDeployments = [c1, c2, c3, c4]
+console.log({mergedDeployments})
 
-const mergedDeployments = merge(c1, c2, c3, c4)
-let data = ''
-let allAddresses = []
+const finalAddrs = mergedDeployments.map(deployment => {
+  const envsAddrs = flatten(Object.values(deployment).map(env => {
+    delete env.network
+    delete env.networkId
+    return Object.values(env)
+  }))
 
-for (const deployment in mergedDeployments) {
-  if (Object.hasOwnProperty.call(mergedDeployments, deployment)) {
-    const releases = mergedDeployments[deployment];
-    delete releases.network
-    delete releases.networkId
-
-    let foreignBridgeValues = []
-    if (releases.ForeignBridge && typeof releases.ForeignBridge === 'object' && releases.ForeignBridge !== null) {
-      foreignBridgeValues = handleObject(releases.ForeignBridge)
-      delete releases.ForeignBridge
-    }
-
-    let homeBridgeValues = []
-    if (releases.HomeBridge && typeof releases.HomeBridge === 'object' && releases.HomeBridge !== null) {
-      homeBridgeValues = handleObject(releases.HomeBridge)
-      delete releases.HomeBridge
-    }
-    if(releases.StakingContracts && typeof releases.StakingContracts === 'object')
+  const finalAddrs = envsAddrs.map(possibleAddr => {
+    if(typeof possibleAddr === 'string')
+      return possibleAddr
+    if(typeof possibleAddr === 'object')
     {
-      releases.StakingContracts = releases.StakingContracts.map(_ => _[0])
-      console.log(releases.StakingContracts)
-    }
-    if(releases.StakingContractsV2 && typeof releases.StakingContractsV2 === 'object')
-    {
-      releases.StakingContractsV2 = releases.StakingContractsV2.map(_ => _[0])
-      console.log(releases.StakingContractsV2)
-    }
+      //handle staking contracts field
+      if(possibleAddr.length>0)
+        return possibleAddr.map(_ => _[0])
 
+      //handle bridge fields
+      return Object.values(possibleAddr).filter(_ => typeof _ === 'string' && _.startsWith('0x'))
+    }
+    return []
+  })
+  return flatten(finalAddrs)
+})
+finalAddrs.push('0x0000000000000000000000000000000000000000')
+const allAddresses = uniq(flatten(finalAddrs).map(_ => _.toLowerCase()))
 
-    let addresses = Object.values(releases)
-    addresses = addresses.concat(foreignBridgeValues)
-    addresses = addresses.concat(homeBridgeValues)
-    // Make sure null address exists in array
-    addresses.push('0x0000000000000000000000000000000000000000')
-    addresses = [...new Set(addresses)]
-    allAddresses = allAddresses.concat(addresses)
-    data = data + `export const ${deployment.replace(/-/g, '_')}: Array<string> = ['${addresses.join('\',\'').toLowerCase()}']\n`
-    // break;
-  }
-}
-data = data + `export const allAddresses: Array<string> = ['${allAddresses.join('\',\'').toLowerCase()}']\n`
+console.log({allAddresses})
+
+const data = `export const allAddresses: Array<string> = ['${allAddresses.join('\',\'').toLowerCase()}']\n`
 writeFileSync(path.resolve('./scripts/releases.ts'), data)
 
-function handleObject(object) {
-  delete object._blockNumber
-  const values = Object.values(object)
-  return values
-}
 // console.log(deployments);
 
